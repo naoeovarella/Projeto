7f7ac09c6aa9d8427fe5ee539ba6901295ab7cb7
naoeovarella <silviocordeirovarella@gmail.com>
2024-05-08 20:23:18 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+    //O DOM é acionado quando todo HTML foi completamente carregado
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Tipos de dados
+    //String: Sequencia de caracter
+    //Number: Números inteiros ou pontos flutuantes (10.4)
+    //BooLean: Retorna dois valores,  true ou false
+    //Object: Estrutura de dados que pode armazenar múltiplos valores
+    //Null: Quando está vazio
+    //Array: Objeto especial ordenado
+    //Function: Bloco de código reutilizável
+   
+    //Const: É usado para declarar uma variável cujo valor é permanente
+    const A = 0;
+
+    //Let: É usado para delcarar variáveis que os valores podem ser retribuidos
+    let B = 0
+
+});
